f4f4ede647403e376de829abec8e4c88580d2fd2
pandeptwidyaop <widya.oktapratama@gmail.com>
2024-04-02 13:10:24 +0800

    update cleanup: add workder

--- internal/cleanup/cleanup.go
+++ internal/cleanup/cleanup.go
@@ -3,16 +3,47 @@ package cleanup
 import (
 	"context"
 	"os"
+	"sync"
 
 	"github.com/pandeptwidyaop/bekup/internal/log"
 	"github.com/pandeptwidyaop/bekup/internal/models"
 )
 
-func Run(ctx context.Context, in <-chan models.BackupFileInfo) <-chan models.BackupFileInfo {
-	return Cleanup(ctx, in)
+func Run(ctx context.Context, in <-chan models.BackupFileInfo, worker int) <-chan models.BackupFileInfo {
+	return cleanupWithWorker(ctx, in, worker)
 }
 
-func Cleanup(ctx context.Context, in <-chan models.BackupFileInfo) <-chan models.BackupFileInfo {
+func cleanupWithWorker(ctx context.Context, in <-chan models.BackupFileInfo, worker int) <-chan models.BackupFileInfo {
+	out := make(chan models.BackupFileInfo)
+
+	wg := sync.WaitGroup{}
+	wg.Add(worker)
+
+	var chans []<-chan models.BackupFileInfo
+
+	for i := 0; i < worker; i++ {
+		chans = append(chans, cleanup(ctx, in))
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	for _, ch := range chans {
+		go func(c <-chan models.BackupFileInfo) {
+			for cc := range c {
+				out <- cc
+			}
+			wg.Done()
+		}(ch)
+	}
+
+	return out
+
+}
+
+func cleanup(ctx context.Context, in <-chan models.BackupFileInfo) <-chan models.BackupFileInfo {
 	out := make(chan models.BackupFileInfo)
 
 	go func() {
